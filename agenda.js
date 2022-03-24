a131dc532e077f6e7b67e4aeeef3e4d040707788
Andres Ponce <and96ponce@gmail.com>
2022-03-24 20:10:32 -0300

    Mi primer commit

---
+++ agenda.js
@@ -0,0 +1,20 @@
+
+
+
+var contacto = new Array;
+
+function contactos(){
+
+    let contactos = document.getElementById("agregar");
+    this.nombre = "";
+    this.apellido = "";
+    this.telefono = "";
+    this.movil = "";
+    this.correo = "";
+    this.direccion = "";
+
+contacto.push(contactos);
+
+}
+
+      
