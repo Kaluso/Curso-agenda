177a154a2fc3aa2b9a1c518069ab7c664bafddc6
Andres Ponce <and96ponce@gmail.com>
2022-04-25 14:43:49 -0400

    plop

--- agenda.js
+++ agenda.js
@@ -10,65 +10,51 @@ var contactos = new Array();
 
 function contacto(){
    
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.telefono = telefono;
-    this.correo = correo;
-    this.direccion = direccion;;
+    this.nombre = '';
+    this.apellido = '';
+    this.telefono = '';
+    this.correo = '';
+    this.direccion = '';
    
 
 }
 
 function agregarContacto(){
 
-       // obtiene los parametros en base al ID del HTML.
-       nombre = document.getElementById('txtNombre').value;
-       apellido = document.getElementById('txtApellido').value;
-       telefono = document.getElementById('txtTelefono').value;
-       correo = document.getElementById('txtCorreo').value;
-       direccion = document.getElementById('txtDireccion').value;
-       
-       // Enlaza la funcion contacto a una variable para ser utilizada a traves del .push del array.
-       let addcontacto = new contacto();
-
-       // genera un arreglo con los parametros enviados a traves de la funcion contacto.
-       contactos.push(addcontacto);
+    let addcontacto = new contacto();
     
-       
-    console.log(contactos);
+     addcontacto.nombre = document.getElementById('txtNombre').value;
+     addcontacto.apellido = document.getElementById('txtApellido').value;
+     addcontacto.telefono = document.getElementById('txtTelefono').value;
+     addcontacto.correo = document.getElementById('txtCorreo').value;
+     addcontacto.direccion = document.getElementById('txtDireccion').value;
+    
+     contactos[contactos.length] = addcontacto;
+      mostrarContactos();
+     contactos.push(addcontacto);
+     
+     
+  
 }
 
+function mostrarContactos(){
+    document.getElementById('todosContactos').innerHTML = '';
+    for (x=0; x<contactos.length; x++){
+        addcontacto = contactos[x];
+        div = document.createElement('div');
+        div.setAttribute('class', 'contactos');
+        div.innerHTML = 
+        addcontacto.nombre+"<br/>"+
+        addcontacto.apellido+"<br/>"+
+        addcontacto.telefono+"<br/>"+
+        addcontacto.correo+"<br/>"+
+        addcontacto.direccion+"<br/>";
+        document.getElementById('todosContactos').appendChild(div);
+       
 
+        
 
-/*
+    }
 
-copypaste del console.log(contactos)
-
-[contacto]
-0: contacto
-apellido: "uso"
-correo: "Prueba@gmail.com"
-direccion: "En el culo de tu vieja"
-nombre: "Kal"
-telefono: "123456789"
-
-*/
-
-/*
-
-function contacto()
-function agregarContacto()
-function eliminarContacto(id)
-function pintarContactos()
-function orderContactos(orden, tipoOrden)
-function filtrarContacto(nombreFiltro, filtro)
-
-
-Pagina usada como referencia 
-https://gist.github.com/Villanuevand/7583485   (Agenda telefonica de alguien)
-
-Video de referencia
-https://www.youtube.com/watch?v=rdJK2vBE3YM&t=1046s&ab_channel=ayudaweb.net   (Agenda telefonica de alguien)
-
-*/
+}
 
